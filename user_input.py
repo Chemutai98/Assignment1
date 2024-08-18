26850858fb5cebb71291a9c0b0f08711662e0b10
Chemutai98 <melicynthia1998@gmail.com>
2024-08-18 22:15:01 +0300

    Answers to week 1 assignment

---
+++ user_input.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+
+
+
+age = input("How old are you? ")
+
+
+location = input("Where do you live? ")
+
+
+print(f"Hello {name}, you are {age} and live in {location}.")
+
